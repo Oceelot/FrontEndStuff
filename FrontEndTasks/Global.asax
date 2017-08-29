b175d895b57064d67259d301b8a82d53e141fbc5
Gary Adams <gary.adams@bizfitech.com>
2017-08-29 07:30:08 +0100

    First commit

---
+++ FrontEndTasks/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FrontEndTasks.MvcApplication" Language="C#" %>
---
+++ FrontEndTasks/Scripts/global/burgerSideMenu.js
@@ -0,0 +1,25 @@
+﻿function ShowSidebar() {
+
+    var sidebar = document.getElementById("sidebar");
+
+    var centerBlocks = document.getElementsByClassName("flexcBlueBlock-center");
+
+    centerBlocks[0].style.color = "white";
+    sidebar.style.width = "300px";
+}
+
+function PopModal() {
+    var modal = document.getElementsByClassName("modal");
+    var screen = document.getElementsByClassName("screen");
+
+    modal[0].style.display = "block";
+    screen[0].style.display = "block";
+}
+
+function CloseModal() {
+    var modal = document.getElementsByClassName("modal");
+    var screen = document.getElementsByClassName("screen");
+
+    modal[0].style.display = "none";
+    screen[0].style.display = "none";
+}
